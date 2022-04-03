de47f41aa8220a91a2a4dd051874b33ead3daec5
hira480 <hiramannan8@gmail.com>
2022-04-03 19:07:29 +0600

    context api with multiple value

--- src/components/Aunty/Aunty.js
+++ src/components/Aunty/Aunty.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react';
 import { RingContext } from '../GrandPa/GrandPa';
 
-const Aunty = ({ house }) => {
-    const ring = useContext(RingContext);
+const Aunty = () => {
+    const [house, setHouse] = useContext(RingContext);
+    const handelHouseIncrease = () => {
+        const newHouseCount = house + 1;
+        setHouse(newHouseCount);
+    }
     return (
         <div>
             <h4>Aunty</h4>
             <p>House: {house}</p>
-            <p>Gift: {ring}</p>
+            <button onClick={handelHouseIncrease}>Aunty Magic</button>
         </div>
     );
 };
